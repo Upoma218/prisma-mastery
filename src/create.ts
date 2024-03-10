d24235f3135ea51556cdeb2e95295a4e1b518db0
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-10 20:58:35 +0600

    Used createMany for creating multiole datas

--- src/create.ts
+++ src/create.ts
@@ -3,14 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  // const result = await prisma.post.create({
-  //   data: {
-  //     title: "this is Title 2",
-  //     content: "this is content 2",
-  //     authorName: "Upoma",
-  //   },
-  // });
-  // console.log(result);
+  //   create many
+
   const createMany = await prisma.post.createMany({
     data: [
       {
