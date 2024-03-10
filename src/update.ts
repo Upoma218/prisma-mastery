a046bd55d3690268dc0291f8581fbf3112f03158
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-10 21:23:51 +0600

    update many

--- src/update.ts
+++ src/update.ts
@@ -3,17 +3,26 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const updates = async () => {
-  const singleUpdate = await prisma.post.update({
+  //   const singleUpdate = await prisma.post.update({
+  //     where: {
+  //       id: 1,
+  //     },
+  //     data: {
+  //       title: "this is title 5",
+  //       content: "this is content 5",
+  //       authorName: "Author 5",
+  //     },
+  //   });
+  //   console.log(singleUpdate);
+  const updateMany = await prisma.post.updateMany({
     where: {
-      id: 1,
+      title: "this is Title 2",
     },
     data: {
-      title: "this is title 5",
-      content: "this is content 5",
-      authorName: "Author 5",
+      published: true,
     },
   });
-  console.log(singleUpdate);
+  console.log(updateMany);
 };
 
 updates();
