e26443c6e22a34ac2ec09ee34167ef8d62e9643c
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-10 22:21:11 +0600

    find all data with cursor based pagination

--- src/pagination-sorting.ts
+++ src/pagination-sorting.ts
@@ -4,11 +4,21 @@ const prisma = new PrismaClient();
 
 const paginationSorting = async () => {
   // offset pagination
-  const offsetData = await prisma.post.findMany({
+  //   const offsetData = await prisma.post.findMany({
+  //     skip: 5,
+  //     take: 3,
+  //   });
+  //   console.log("offsetData", offsetData);
+
+  // cusrsor based pagination
+  const cursorBasedPagination = await prisma.post.findMany({
     skip: 5,
     take: 3,
+    cursor: {
+      id: 30,
+    },
   });
-  console.log("offsetData", offsetData);
+  console.log("cursorBasedPagination", cursorBasedPagination);
 };
 
 paginationSorting();
