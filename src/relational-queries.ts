29cec48c28bd6b971256bcdda864855eccdd9451
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-11 02:46:28 +0600

    fluent api

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -3,15 +3,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const relationalQueries = async () => {
+  // const result = await prisma.user.findUnique({
+  //   where: {
+  //     id: 1,
+  //   },
+  //   include: {
+  //     post: true,
+  //   },
+  // });
   // fluent api
-  const result = await prisma.user.findUnique({
-    where: {
-      id: 1,
-    },
-    include: {
-      post: true,
-    },
-  });
+  const result = await prisma.user
+    .findUnique({
+      where: {
+        id: 1,
+      },
+    })
+    .post();
+
   // const result = await prisma.user.findUnique({
   //     where: {
   //         id: 1
