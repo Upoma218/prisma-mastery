32c6b5c736b330e2c1cce0e0615867c31c21e02c
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-10 20:43:14 +0600

    find, findFast,findUnique

---
+++ src/find.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // find all
+  const getAllFromDB = await prisma.post.findMany();
+
+  // find fast or find fast throw error
+  const findFast = await prisma.post.findFirstOrThrow({
+    where: {
+      published: false,
+    },
+  });
+
+  // find unique or find fast throw unique
+  const findUnique = await prisma.post.findUnique({
+    where: {
+      id: 2,
+    },
+  });
+  console.log({ findUnique });
+};
+
+main();
